a6c892824ca7f5584de2c2ce8e24d2eabcf09f2c
Gao <1520045270@qq.com>
2021-05-17 21:03:58 +0800

    跑通时域的检测描述接口

--- src/views/components/Voice/VoiceDetectResultAnalysis/VoiceDetectResultAnalysisDetail.vue
+++ src/views/components/Voice/VoiceDetectResultAnalysis/VoiceDetectResultAnalysisDetail.vue
@@ -4,12 +4,12 @@
     <a-row>
       <a-col :span="3">
         轴承编号<br/>
-        <a-input v-model:value="detect_time" placeholder="1号车轴" />
+        <a-input v-model:value="devicesno" placeholder="1号车轴" />
       </a-col>
       <a-col :span="1"></a-col>
       <a-col :span="3">
         轴承类型<br/>
-        <a-input v-model:value="detect_time" placeholder="圆柱滚子轴承" />
+        <a-input v-model:value="devicecategory" placeholder="圆柱滚子轴承" />
       </a-col>
       <a-col :span="1"></a-col>
       <a-col :span="2">
@@ -18,13 +18,13 @@
       <a-col :span="1"></a-col>
       <a-col :span="3">
         使用时间<br/>
-        <a-input v-model:value="detect_time" placeholder="2年11月" />
+        <a-input v-model:value="devicecategory" placeholder="2年11月" />
       </a-col>
       <a-col :span="3"></a-col>
       <a-col :span="7">
         上次更新时间:2020-10-31 11:40<br/>
-        <a-button type="primary">
-          停止检测
+        <a-button type="primary" @click="()=>handleDetectChange(isDetecting)">
+          {{isDetecting?'停止检测':'开始检测'}}
         </a-button>&nbsp;&nbsp;&nbsp;
         <a-button type="primary">
           <router-link to="/voice_fault_print">
@@ -50,6 +50,47 @@ import Breadcrumb from "../../../components/Common/Breadcrumb.vue";
 import VoiceDetectResultAnalysisDetailTable from "./VoiceDetectResultAnalysisDetailTable.vue";
 import VoiceDetectResultAnalysisDetailWave from "./VoiceDetectResultAnalysisDetailWave.vue";
 import VoiceDetectResultAnalysisDetailAnalysis from "./VoiceDetectResultAnalysisDetailAnalysis.vue";
+
+import { reactive, toRefs } from "vue";
+import { get } from "../../../../utils/request.js";
+//声信号检测结果相关逻辑
+const voiceDetectResultEffect = () => {
+  const data = reactive({
+    isDetecting: false,
+    devicesno: "",
+    devicecategory: ""
+  });
+  const handleDetectChange = async isDetecting => {
+    if (!data.isDetecting) {
+      const result = await get(
+        `https://result.eolinker.com/8WmLt3ib3418debd511d5eee42ae1e659a3307d6da1de4d?uri=/database/startdevicedecetion/${devicecategory}/${devicesno}`
+      );
+      if (result.retCode == 200) {
+        console.log("开始检测");
+        data.isDetecting = !isDetecting;
+      }
+    } else {
+      const result = await get(
+        `https://mockapi.eolinker.com/8WmLt3ib3418debd511d5eee42ae1e659a3307d6da1de4d/database/stopdevicedecetion/${devicecategory}/${devicesno}`
+      );
+      if (result.retCode == 200) {
+        console.log("结束检测");
+        data.isDetecting = !isDetecting;
+      }
+      else{
+        console.log(result)
+      }
+    }
+  };
+  const { isDetecting, devicesno, devicecategory } = toRefs(data);
+  return {
+    isDetecting,
+    devicesno,
+    devicecategory,
+    handleDetectChange
+  };
+};
+
 export default {
   components: {
     Breadcrumb,
@@ -57,9 +98,24 @@ export default {
     VoiceDetectResultAnalysisDetailWave,
     VoiceDetectResultAnalysisDetailAnalysis
   },
-  data() {
+  // data() {
+  //   return {
+  //     detect_time: ""
+  //   };
+  // }
+
+  setup() {
+    const {
+      isDetecting,
+      devicesno,
+      devicecategory,
+      handleDetectChange
+    } = voiceDetectResultEffect();
     return {
-      detect_time: ""
+      isDetecting,
+      devicesno,
+      devicecategory,
+      handleDetectChange
     };
   }
 };
