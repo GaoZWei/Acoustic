6eece93e6ee46b77f596fa8f9e6629bdce916bec
Gao <1520045270@qq.com>
2021-09-16 21:48:24 +0800

    优化注册页代码

--- src/views/components/Common/Register.vue
+++ src/views/components/Common/Register.vue
@@ -5,22 +5,34 @@
         <a-row>
             <a-col :span="9"></a-col>
             <a-col :span="6">
-                <a-form :model="form" :label-col="labelCol" :wrapper-col="wrapperCol">
+                <!-- <a-form ref="formRef" :rules="rules" :label-col="labelCol" :wrapper-col="wrapperCol"> -->
+                <a-form :label-col="labelCol" :wrapper-col="wrapperCol">
                     <!-- <a-form-item label="账号"> -->
                     <a-form-item>
-                        <a-input v-model:value="form.email" placeholder="邮箱" />
+                        <a-input v-model:value="email" type="email" placeholder="邮箱/用户名">
+                            <template #prefix>
+                                <UserOutlined />
+                            </template>
+                        </a-input>
                     </a-form-item>
                     <a-form-item block>
-                        <a-input-password v-model:value="form.password" placeholder="6-16位密码，区分大小写" />
+                        <a-input-password v-model:value="password" autocomplete="new-password" placeholder="6-16位密码，区分大小写">
+                            <template #prefix>
+                                <LockOutlined/>
+                            </template>
+                        </a-input-password>
                     </a-form-item>
                     <a-form-item block>
-                        <a-input-password v-model:value="form.password" placeholder="确认密码" />
+                        <a-input-password v-model:value="validpassword" placeholder="确认密码">
+                            <template #prefix>
+                                <LockOutlined/>
+                            </template>
+                        </a-input-password>
                     </a-form-item>
                     <a-form-item block>
                         <!-- <a-input v-model:value="form.phone_number" placeholder="11位手机号" /> -->
-
                         <a-input-group compact>
-                            <a-select v-model:value="form.phone_pre" style="width: 30%" @focus="focus" ref="select" @change="handleChange">
+                            <a-select v-model:value="phone_pre" style="width: 30%" @focus="focus" ref="select" @change="handleChange">
                                 <a-select-option value="+86">
                                     +86
                                 </a-select-option>
@@ -29,13 +41,13 @@
                                 </a-select-option>
                             </a-select>
                             <!-- <a-input style="width: 50%" v-model:value="value6" /> -->
-                            <a-input v-model:value="form.phone_number" style="width:70%" placeholder="11位手机号" />
+                            <a-input v-model:value="phone_number" style="width:70%" placeholder="11位手机号" />
                         </a-input-group>
                     </a-form-item>
                     <a-row>
                         <a-col :span="15">
                             <a-form-item>
-                                <a-input v-model:value="form.phone_validation" placeholder="输入验证码" />
+                                <a-input v-model:value="phone_validation" placeholder="输入验证码" />
                             </a-form-item>
                         </a-col>
                         <a-col :span="2"></a-col>
@@ -49,15 +61,13 @@
                     <a-row>
                         <a-col :span="9">
                             <a-form-item>
-                                <a-button type="primary" @click="onSubmit" block>
-                                    <router-link to="/">注册</router-link>
-                                </a-button>
+                                <a-button type="primary" @click="handleRegisterClick" block>注册</a-button>
                             </a-form-item>
                         </a-col>
                         <a-col :span="6"></a-col>
                         <a-col :span="9">
                             <div class="block_item_register"></div>
-                            <a href="/login">使用已有账户登录</a>
+                            <div class="block_item_register" @click="handleLoginClick">使用已有账户登录</div>
                         </a-col>
                     </a-row>
                 </a-form>
@@ -67,20 +77,109 @@
     </div>
 </template>
 <script>
+import { reactive, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import { UserOutlined, LockOutlined } from "@ant-design/icons-vue";
+
+//注册相关逻辑
+const useRegisterEffect = () => {
+  const router = useRouter();
+  const data = reactive({
+    email: "",
+    password: "",
+    validpassword: "",
+    phone_number: "",
+    phone_validation: "",
+    phone_pre: "+86"
+  });
+  const handleRegisterClick = () => {
+    const {
+      // email,
+      password,
+      validpassword,
+      phone_number
+      //   phone_validation,
+      //   phone_pre
+    } = data;
+    // const formRef = toRef();
+    // formRef.value.validate();
+    if (password != validpassword) {
+      alert("两次密码不相同");
+    }
+    if (phone_number.length > 11) {
+      alert("手机号不合法");
+    } else {
+      router.push({ name: "login" });
+    }
+  };
+  const {
+    email,
+    password,
+    validpassword,
+    phone_number,
+    phone_validation,
+    phone_pre
+  } = toRefs(data);
+  return {
+    handleRegisterClick,
+    email,
+    password,
+    validpassword,
+    phone_number,
+    phone_validation,
+    phone_pre
+  };
+};
+//登录跳转逻辑
+const useLoginEffect = () => {
+  const router = useRouter();
+  const handleLoginClick = () => {
+    router.push({ name: "login" });
+  };
+  return { handleLoginClick };
+};
 export default {
   name: "Login",
-  data() {
+  components: {
+    UserOutlined,
+    LockOutlined
+  },
+  setup() {
+    // const rules = {
+    //   email: [
+    //     {
+    //       required: true,
+    //       message: "Please input Activity name",
+    //       trigger: "blur"
+    //     }
+    //   ]
+    // };
+    var labelCol = { span: 12 };
+    var wrapperCol = { span: 24 };
+    var current = ["mail"];
+    const {
+      handleRegisterClick,
+      email,
+      password,
+      validpassword,
+      phone_number,
+      phone_validation,
+      phone_pre
+    } = useRegisterEffect();
+    const { handleLoginClick } = useLoginEffect();
     return {
-      current: ["mail"],
-      labelCol: { span: 12 },
-      wrapperCol: { span: 24 },
-      form: {
-        email: "",
-        password: "",
-        phone_number: "",
-        phone_validation: "",
-        phone_pre: "+86"
-      }
+      handleRegisterClick,
+      handleLoginClick,
+      email,
+      password,
+      validpassword,
+      phone_number,
+      phone_validation,
+      phone_pre,
+      labelCol,
+      wrapperCol,
+      current
+      //   rules
     };
   },
   methods: {
@@ -122,6 +221,7 @@ export default {
 .block_item_register {
   margin-top: 3%;
   height: 4%;
+  color: rgba(24, 144, 255, 1);
 }
 .get_phone_code {
   font-size: 0.8em;
